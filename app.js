1118376509b1f1b3dedc9b824e1731a46c341587
Adnan Usman <adnantu@hotmail.com>
2019-02-28 21:28:12 -0800

    Added Yargs and Option to input address

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 require('dotenv').config();
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+.options({
+  a: {
+    demand: true,
+    description: '',
+    alias: 'address',
+    string: true  
+  }
+})
+.help()
+.alias('help', 'h')
+.argv
 
 const key = process.env.key;
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=9744%20Westminster%20Ave&key=${key}`,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.a)}&key=${key}`,
   json: true
 }, (error, response, body) => {
   console.log(JSON.stringify(response, undefined, 2));
