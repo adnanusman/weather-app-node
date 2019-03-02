8f2fcccdc3c7dd116adb79ed5a913f3c3fa74b12
Adnan Usman <adnantu@hotmail.com>
2019-03-01 17:09:23 -0800

    Switched to Mapbox API

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const request = require('request');
 const yargs = require('yargs');
 
+const token = process.env.token;
+
 const argv = yargs
 .options({
   a: {
@@ -15,11 +17,11 @@ const argv = yargs
 .alias('help', 'h')
 .argv
 
-const key = process.env.key;
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.a)}&key=${key}`,
+  url: `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(argv.a)}.json?access_token=${token}`,
   json: true
-}, (error, response, body) => {
-  console.log(JSON.stringify(response, undefined, 2));
+}, (error, response) => {
+  console.log(`Longitude: ${response.body.features[0].geometry.coordinates[0]}`);
+  console.log(`Latitude: ${response.body.features[0].geometry.coordinates[1]}`)  
 })
